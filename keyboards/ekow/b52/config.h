44171960a710c3e5cb3fce9baa973f0b2ee4efa3
eugenepy <tn00372136@gmail.com>
2022-07-24 18:37:09 +0800

    da

---
+++ keyboards/ekow/b52/config.h
@@ -0,0 +1,27 @@
+// Copyright 2022 ekow (@eugenepy)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID 0x4B59 // KY
+#define PRODUCT_ID 0x0002
+#define DEVICE_VER 0x0001
+#define MANUFACTURER KeeBoyzLab
+#define PRODUCT B52
+
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
