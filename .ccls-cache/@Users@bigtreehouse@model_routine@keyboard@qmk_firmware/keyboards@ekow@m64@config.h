a27a927d8a6e38518cfedee9153035dea43364a8
eugenepy <tn00372136@gmail.com>
2023-01-15 22:26:15 +0800

    m64 release

--- .ccls-cache/@Users@bigtreehouse@model_routine@keyboard@qmk_firmware/keyboards@ekow@m64@config.h
+++ .ccls-cache/@Users@bigtreehouse@model_routine@keyboard@qmk_firmware/keyboards@ekow@m64@config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID 0x0000
 #define DEVICE_VER 0x0001
 #define MANUFACTURER KeeBoyzLab
-#define PRODUCT M64
+#define PRODUCT C64
 
 // key matrix size
 #define MATRIX_ROWS 5
@@ -32,9 +32,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // M64 PCB default pin-out
 #define MATRIX_ROW_PINS \
-    { B10, A15, B1, B0, A4 }
+    { B10, A2, A5, A6, A7 }
 #define MATRIX_COL_PINS \
-    { A1, B12, A8, B13, A10, A13, B4, B3, B5, B8, B9, C13, C14, C15, A0 }
+    { A1, B12, B13, B14, A15, B15, B3, B4, B5, B8, B9, C13, C14, C15, A10 }
 
 // Keyboard Matrix Assignments
 #define DIODE_DIRECTION COL2ROW
@@ -44,7 +44,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define ISS_SDB_PIN A3
 #define DRIVER_COUNT 1
 #define DRIVER_ADDR_1 0b0110000
-#define LED_DRIVER_1_LED_TOTAL 1
+#define LED_DRIVER_1_LED_TOTAL 70
 #define DRIVER_LED_TOTAL LED_DRIVER_1_LED_TOTAL
 #define ISSI_DRIVER_TOTAL DRIVER_LED_TOTAL
 
@@ -101,18 +101,30 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // RGB Underglow
 /* M64 using PWD to control the underglow.
  */
-#define RGB_DI_PIN B15
+#define RGB_DI_PIN A8
 #define RGBLED_NUM 16
 #define RGBLIGHT_LED_MAP \
     { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0 }
 
+#define RGBLIGHT_EFFECT_BREATHING
+#define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#define RGBLIGHT_EFFECT_SNAKE
+#define RGBLIGHT_EFFECT_KNIGHT
+#define RGBLIGHT_EFFECT_CHRISTMAS
+#define RGBLIGHT_EFFECT_STATIC_GRADIENT
 #define RGBLIGHT_EFFECT_RGB_TEST
+#define RGBLIGHT_EFFECT_ALTERNATING
+#define RGBLIGHT_EFFECT_TWINKLE
 
-#define WS2812_PWM_COMPLEMENTARY_OUTPUT
+#define RGBLIGHT_DEFAULT_HUE 10
+#define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_RAINBOW_MOOD
+
+// #define WS2812_PWM_COMPLEMENTARY_OUTPUT
 #define WS2812_PWM_DRIVER PWMD1
-#define WS2812_PWM_CHANNEL 3
-
+#define WS2812_PWM_CHANNEL 1
 #define WS2812_PWM_PAL_MODE 1
+
 #define WS2812_DMA_STREAM STM32_DMA2_STREAM5
 #define WS2812_DMA_CHANNEL 6
 
--- .ccls-cache/@Users@bigtreehouse@model_routine@keyboard@qmk_firmware/keyboards@ekow@m64@m64.c
+++ .ccls-cache/@Users@bigtreehouse@model_routine@keyboard@qmk_firmware/keyboards@ekow@m64@m64.c
@@ -27,7 +27,74 @@ void board_init(void) {
 }
 
 const is31_led PROGMEM g_is31_leds[DRIVER_LED_TOTAL] = {
-    {0, CS12_SW1, CS11_SW1, CS10_SW1}, /* RGB1 */
+    {0, CS27_SW8, CS26_SW8, CS25_SW8},
+    {0, CS27_SW1, CS26_SW1, CS25_SW1},
+    {0, CS27_SW2, CS26_SW2, CS25_SW2},
+    {0, CS27_SW3, CS26_SW3, CS25_SW3},
+    {0, CS27_SW4, CS26_SW4, CS25_SW4},
+    {0, CS27_SW5, CS26_SW5, CS25_SW5},
+    {0, CS27_SW6, CS26_SW6, CS25_SW6},
+    {0, CS27_SW7, CS26_SW7, CS25_SW7},
+    {0, CS2_SW8, CS3_SW8, CS1_SW8},
+    {0, CS2_SW1, CS3_SW1, CS1_SW1},
+    {0, CS2_SW2, CS3_SW2, CS1_SW2},
+    {0, CS2_SW3, CS3_SW3, CS1_SW3},
+    {0, CS2_SW4, CS3_SW4, CS1_SW4},
+    {0, CS2_SW5, CS3_SW5, CS1_SW5},
+    {0, CS2_SW6, CS3_SW6, CS1_SW6},
+    {0, CS2_SW7, CS3_SW7, CS1_SW7}, {0, CS30_SW8, CS29_SW8, CS28_SW8}, {0, CS30_SW1, CS29_SW1, CS28_SW1},
+    {0, CS30_SW2, CS29_SW2, CS28_SW2},
+    {0, CS30_SW3, CS29_SW3, CS28_SW3},
+    {0, CS30_SW4, CS29_SW4, CS28_SW4},
+    {0, CS30_SW5, CS29_SW5, CS28_SW5},
+    {0, CS30_SW6, CS29_SW6, CS28_SW6},
+    {0, CS30_SW7, CS29_SW7, CS28_SW7},
+    {0, CS5_SW8, CS6_SW8, CS4_SW8},
+    {0, CS5_SW1, CS6_SW1, CS4_SW1},
+    {0, CS5_SW2, CS6_SW2, CS4_SW2},
+    {0, CS5_SW3, CS6_SW3, CS4_SW3},
+    {0, CS5_SW4, CS6_SW4, CS4_SW4},
+    {0, CS5_SW5, CS6_SW5, CS4_SW5},
+    {0, CS5_SW6, CS6_SW6, CS4_SW6},
+    {0, CS36_SW8, CS35_SW8, CS34_SW8},
+    {0, CS36_SW1, CS35_SW1, CS34_SW1},
+    {0, CS36_SW2, CS35_SW2, CS34_SW2},
+    {0, CS36_SW3, CS35_SW3, CS34_SW3},
+    {0, CS36_SW4, CS35_SW4, CS34_SW4},
+    {0, CS36_SW5, CS35_SW5, CS34_SW5},
+    {0, CS36_SW6, CS35_SW6, CS34_SW6},
+    {0, CS36_SW7, CS35_SW7, CS34_SW7},
+    {0, CS16_SW8, CS15_SW8, CS14_SW8},
+    {0, CS16_SW1, CS15_SW1, CS14_SW1},
+    {0, CS16_SW2, CS15_SW2, CS14_SW2},
+    {0, CS16_SW3, CS15_SW3, CS14_SW3},
+    {0, CS16_SW4, CS15_SW4, CS14_SW4},
+    {0, CS16_SW5, CS15_SW5, CS14_SW5},
+    {0, CS33_SW8, CS32_SW8, CS31_SW8},
+    {0, CS33_SW1, CS32_SW1, CS31_SW1},
+    {0, CS33_SW2, CS32_SW2, CS31_SW2},
+    {0, CS33_SW3, CS32_SW3, CS31_SW3},
+    {0, CS33_SW4, CS32_SW4, CS31_SW4},
+    {0, CS33_SW5, CS32_SW5, CS31_SW5},
+    {0, CS33_SW6, CS32_SW6, CS31_SW6},
+    {0, CS33_SW7, CS32_SW7, CS31_SW7},
+    {0, CS10_SW8, CS9_SW8, CS8_SW8},
+    {0, CS10_SW1, CS9_SW1, CS8_SW1},
+    {0, CS10_SW2, CS9_SW2, CS8_SW2},
+    {0, CS10_SW3, CS9_SW3, CS8_SW3},
+    {0, CS10_SW4, CS9_SW4, CS8_SW4},
+    {0, CS10_SW5, CS9_SW5, CS8_SW5},
+    {0, CS10_SW6, CS9_SW6, CS8_SW6},
+    {0, CS24_SW8, CS23_SW8, CS22_SW8},
+    {0, CS24_SW1, CS23_SW1, CS22_SW1},
+    {0, CS24_SW2, CS23_SW2, CS22_SW2},
+    {0, CS24_SW3, CS23_SW3, CS22_SW3},
+    {0, CS24_SW6, CS23_SW6, CS22_SW6},
+    {0, CS13_SW2, CS12_SW2, CS11_SW2},
+    {0, CS13_SW3, CS12_SW3, CS11_SW3},
+    {0, CS13_SW4, CS12_SW4, CS11_SW4},
+    {0, CS13_SW5, CS12_SW5, CS11_SW5},
+    {0, CS13_SW6, CS12_SW6, CS11_SW6},
 };
 
 void set_color(rgb_color_t *color, uint8_t r, uint8_t g, uint8_t b) {
@@ -36,14 +103,24 @@ void set_color(rgb_color_t *color, uint8_t r, uint8_t g, uint8_t b) {
     color->b = g;
 }
 
-led_config_t g_led_config = {{
-                                 /* C0       C1       C2       C3       C4       C5       C6       C7       C8       C9       C10       C11       C12       C13       C14       C15 */
-                                 {0},
+led_config_t g_led_config = {
+{
+    { 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14}, 
+    {16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30}, 
+    {31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 15}, 
+    {45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59}, 
+    {60, 61, 62, 63, 64, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED,  65, 66, 67, 68, 69}
+
+},
+                            {
+    {0, 0}, {28, 0}, {41, 0}, {55, 0}, {68, 0}, {82, 0}, 
+    {96, 0}, {109, 0}, {123, 0}, {136, 0}, {150, 0}, 
+    {163, 0}, {177, 0}, {191, 0}, {204, 0}, {218, 0}, 
+    {0, 16}, {28, 16}, {48, 16}, {62, 16}, {75, 16}, 
+    {89, 16}, {102, 16}, {116, 16}, {129, 16}, {143, 16}, 
+    {157, 16}, {170, 16}, {184, 16}, {197, 16}, {211, 16}, {0, 32}, {28, 32}, {51, 32}, {65, 32}, {79, 32}, {92, 32}, {106, 32}, {119, 32}, {133, 32}, {146, 32}, {160, 32}, {174, 32}, {187, 32}, {201, 32}, {0, 48}, {28, 48}, {58, 48}, {72, 48}, {85, 48}, {99, 48}, {112, 48}, {126, 48}, {140, 48}, {153, 48}, {167, 48}, {180, 48}, {194, 48}, {211, 48}, {224, 48}, {0, 64}, {28, 64}, {45, 64}, {62, 64}, {79, 64}, {163, 64}, {180, 64}, {197, 64}, {211, 64}, {224, 64}
                              },
-                             {
-                                 {0, 0} // 15
-                             },
-                             {8}};
+{8, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4}};
 #endif
 
 #ifdef CONSOLE_ENABLE
@@ -62,6 +139,21 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 #endif
 
 #if defined(CAP_INDICATOR_ENABLE) || defined(LAYER_INDICATOR_ENABLE)
+<<<<<<< Updated upstream
+=======
+/*
+bool led_update_kb(led_t led_state) {
+    if (led_update_user(led_state)) {
+        if (led_state.caps_lock) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    return false;
+}
+*/
+>>>>>>> Stashed changes
 
 void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     // Default RED
